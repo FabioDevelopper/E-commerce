d523522ed721675b78a492625f6ca20e6e4fb178
FabioDéveloppeur <fabrigaskpogo1427@gmail.com>
2026-05-31 11:27:01 +0000

    Suppression des slash au début concernés exemple: /Images/.... En Images/.... car sur github, il ne reconnait pas ce chemin

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const Title=document.querySelector("#title");
 const Description=document.querySelector("#description");
 let index=0;
 const mypiture =[ {
-    URL:"/Images/image\ \(1\).jpg", title:"Bienvenue", description:"Découvrez nos sercives professionnels."
+    URL:"Images/image\ \(1\).jpg", title:"Bienvenue", description:"Découvrez nos sercives professionnels."
 }, {
     URL:"/Images/image\ \(2\).jpg",title:"Dévéloppement web", description:"créaction des sites webs professionnels , modernes et responsives."
 },{
-    URL:"/Images/image\ \(3\).jpg",title:"Nos partenaires", description:"Université de Lomé, Université de Kara, ESGIS, Ecobank, Orabank....."
+    URL:"Images/image\ \(3\).jpg",title:"Nos partenaires", description:"Université de Lomé, Université de Kara, ESGIS, Ecobank, Orabank....."
 },{
-    URL:"/Images/image\ \(4\).jpg",title:"Nos Actualités", description:"Découvrez nos  Sorties et Dons. "
+    URL:"Images/image\ \(4\).jpg",title:"Nos Actualités", description:"Découvrez nos  Sorties et Dons. "
 },{
-    URL:"/Images/image\ \(5\).jpg",title:"A propos de Nous", description:"Connaissez-vous FabiolDell."
+    URL:"Images/image\ \(5\).jpg",title:"A propos de Nous", description:"Connaissez-vous FabiolDell."
 },{
-    URL:"/Images/image\ \(6\).jpg",title:"Nos Projets & Rêves ", description:"Imaginez un peu ce que deviendra nos entreprises dans quelques années."
+    URL:"Images/image\ \(6\).jpg",title:"Nos Projets & Rêves ", description:"Imaginez un peu ce que deviendra nos entreprises dans quelques années."
 },{
-    URL:"/Images/image\ \(7\).jpg",title:"Nous rejoindre", description:"Veuillez vous contacter via......."
+    URL:"Images/image\ \(7\).jpg",title:"Nous rejoindre", description:"Veuillez vous contacter via......."
 }];
 setInterval(()=>{
     index++;
