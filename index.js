05cea7631d20257a49e9416eb8741ffc80096926
FabioDéveloppeur <fabrigaskpogo1427@gmail.com>
2026-06-01 02:12:50 +0000

    Modification de la page index.html en créant son propre style de nom page....css et aussi certaines modifications

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const mypiture =[ {
     URL:"/Images/image\ \(2\).jpg",title:"Dévéloppement web", description:"créaction des sites webs professionnels , modernes et responsives."
 },{
     URL:"Images/image\ \(3\).jpg",title:"Nos partenaires", description:"Université de Lomé, Université de Kara, ESGIS, Ecobank, Orabank....."
-},{
-    URL:"Images/image\ \(4\).jpg",title:"Nos Actualités", description:"Découvrez nos  Sorties et Dons. "
-},{
-    URL:"Images/image\ \(5\).jpg",title:"A propos de Nous", description:"Connaissez-vous FabiolDell."
-},{
-    URL:"Images/image\ \(6\).jpg",title:"Nos Projets & Rêves ", description:"Imaginez un peu ce que deviendra nos entreprises dans quelques années."
-},{
-    URL:"Images/image\ \(7\).jpg",title:"Nous rejoindre", description:"Veuillez vous contacter via......."
-}];
+// },{
+//     URL:"Images/image\ \(4\).jpg",title:"Nos Actualités", description:"Découvrez nos  Sorties et Dons. "
+// },{
+//     URL:"Images/image\ \(5\).jpg",title:"A propos de Nous", description:"Connaissez-vous FabiolDell."
+// },{
+//     URL:"Images/image\ \(6\).jpg",title:"Nos Projets & Rêves ", description:"Imaginez un peu ce que deviendra nos entreprises dans quelques années."
+// },{
+//     URL:"Images/image\ \(7\).jpg",title:"Nous rejoindre", description:"Veuillez vous contacter via......."
+ }];
 setInterval(()=>{
     index++;
      if(index >=mypiture.length){
@@ -29,11 +29,11 @@ setInterval(()=>{
     Content.style.animation="none";
 
     setTimeout(()=>{
-        Content.style.animation= "deplacement 5s";
+        Content.style.animation= "deplacement 10s";
 
     },10);
     
-},5000);
+},7000);
 // section pour le caseroul
  
 
